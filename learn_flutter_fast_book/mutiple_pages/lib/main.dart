dd76f356cd317f9754ff2c2729ba369ed2e53088
Wale Olaleye <2972971+waleo@users.noreply.github.com>
2023-08-06 20:18:54 -0400

    Implement a buttons page.
    * Remove Generator page since it it no longer needed.

--- learn_flutter_fast_book/mutiple_pages/lib/main.dart
+++ learn_flutter_fast_book/mutiple_pages/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/buttons_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,22 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Multi Page App'),
@@ -54,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     switch (selectedIndex) {
       case 0:
-        page = GeneratorPage();
+        page = const ButtonsPage();
         break;
       case 1:
         page = const Placeholder();
@@ -71,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
           extended: true,
           destinations: const [
             NavigationRailDestination(
-                icon: Icon(Icons.home), label: Text('Buttons')),
+                icon: Icon(Icons.smart_button_sharp), label: Text('Buttons')),
             NavigationRailDestination(
                 icon: Icon(Icons.favorite), label: Text('Page 2'))
           ],
@@ -89,40 +75,5 @@ class _MyHomePageState extends State<MyHomePage> {
         ))
       ],
     ));
-  }
-}
-
-class GeneratorPage extends StatelessWidget {
-  const GeneratorPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  // appState.toggleFavorite();
-                },
-                icon: const Icon(Icons.abc),
-                label: Text('Like'),
-              ),
-              const SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  // appState.getNext();
-                },
-                child: const Text('Next'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
   }
 }
---
+++ learn_flutter_fast_book/mutiple_pages/lib/pages/buttons_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class ButtonsPage extends StatelessWidget {
+  const ButtonsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Row textButtonRow =
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      TextButton(
+          onPressed: () => debugPrint('textbutton pressed'),
+          child: const Text('TextButton')),
+    ]);
+    Row elevatedButtonRow =
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      ElevatedButton(
+          onPressed: () => debugPrint('ElevatedButton pressed'),
+          child: const Text('ElevatedButton')),
+    ]);
+    Row iconButtonRow =
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      IconButton(
+          icon: const Icon(Icons.add),
+          onPressed: () => debugPrint('IconButton pressed')),
+      const Text('IconButton'),
+    ]);
+    Row outlinedButtonRow =
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      OutlinedButton(
+          onPressed: () => debugPrint('OutlinedButton pressed'),
+          child: const Text('OutlinedButton')),
+    ]);
+    Row dropdownButtonRow =
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      DropdownButton(items: const [
+        DropdownMenuItem(value: 'Mens', child: Text('Mens')),
+        DropdownMenuItem(value: 'Womens', child: Text('Womens'))
+      ], onChanged: (value) => debugPrint('Changed: $value')),
+      const Text('Dropdown Button')
+    ]);
+    Row backButtonRow = const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [BackButton(), Text('BackButton')]);
+
+    Row closeButtonRow = const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [CloseButton(), Text('CloseButton')]);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Buttons')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            textButtonRow,
+            elevatedButtonRow,
+            iconButtonRow,
+            outlinedButtonRow,
+            dropdownButtonRow,
+            backButtonRow,
+            closeButtonRow
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => debugPrint('FloatingActionButton pressed'),
+        child: const Text('F.A.B'),
+      ),
+    );
+  }
+}
